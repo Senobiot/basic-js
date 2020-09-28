6aada292887ae86aa1bf4351f2a28bbdd1c23467
Aliaksandr Kulik <inter_net@tut.by>
2020-09-28 22:49:02 +0300

    feat: add 3 first solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+	let dt = "";
+	  if (typeof arr !== "object" ||  arr === null) {
+    return false
+  }
+	for (var i = 0; i < arr.length; i++) {
+		if (typeof arr[i] === "string") {
+			dt +=arr[i].trim().charAt(0).toUpperCase();
+		}
+	}
+	return dt.split("").sort().join("");
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, obj) {
+	let tms = obj.repeatTimes,
+		sep = obj.separator,
+		add = obj.addition, 
+		addTms = obj.additionRepeatTimes,
+		addSep = obj.additionSeparator;
+	str = String(str);
+	if (add === null || typeof add === 'boolean') add = String(add);
+	if (!sep) {sep = "+"};
+	if (!tms) {tms = 1};
+
+	if (add && addTms && addSep) {str +=(add + addSep).repeat(addTms).slice(0, -addSep.length)}
+	else if (add && addTms) {str +=add.repeat(addTms)}
+	else if (add) {str +=add};
+	return (str + sep).repeat(tms).slice(0, -sep.length);
 };
-  
