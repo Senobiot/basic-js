ec3bc9011e1bd9aa509cc799e59047ae27e0b221
Aliaksandr Kulik <inter_net@tut.by>
2020-10-06 03:15:36 +0300

    feat: all done

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+   chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    String(value).length === 0 ? this.chain.push("()"):
+    this.chain.push(`( ${value} )`);
+      return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      if (!this.chain[position - 1]) { 
+        this.chain = [];
+        throw new Error("error")};
+      this.chain.splice(position - 1, 1);
+      return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.chain.join("~~");
+    this.chain = []
+    return result;
   }
 };
 
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,64 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+ constructor(type = true) {
+ 	if (type) {
+ 		this.type = 'direct';
+ 	} else {
+ 		this.type = 'reverse';
+ 	}
+  }
+
+
+  encrypt(enc, cipher) {
+
+  	  	if (!enc || !cipher) throw Error("Invalid argument");
+    let table = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        str = enc.toUpperCase(),
+        code = cipher.toUpperCase(),
+        j = 0,
+        result = "";
+
+     let ind = (phrase, index) => table.indexOf(phrase[index]);
+
+
+    for (let i = 0; i < str.length; i++) {
+      if (ind(str, i) >= 0) {
+        if (ind(code, j) + ind(str, i) >= 26) {
+          result += table[ind(code, j) + ind(str, i) - 26]
+        } else {result += table[ind(code, j) + ind(str, i)]}
+
+			j >= code.length - 1 ? j = 0 : j++;
+      }
+      else {result += str[i]}
+    } 
+return this.type === 'reverse'? result.split('').reverse().join(''): result;
+
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(enc, cipher) {
+
+  	  		if (!enc || !cipher) throw Error("Invalid argument");
+    let table = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        str = enc.toUpperCase(),
+        code = cipher.toUpperCase(),
+        j = 0,
+        result = "";
+    
+    for (let i = 0; i < str.length; i++) {
+      if (table.indexOf(str[i]) >= 0) {
+        if ((table.indexOf(str[i]) - table.indexOf(code[j])) >= 0) {
+          result += table[table.indexOf(str[i]) - table.indexOf(code[j])]
+        } else {result += table[table.indexOf(str[i])  - table.indexOf(code[j]) + 26]
+               }
+
+        if (j >= code.length - 1) {j = 0}
+        else {j++}   
+      }
+      else {result += str[i]}
+    } 
+
+	return this.type === 'reverse'? result.split('').reverse().join(''): result;
   }
 }
 
